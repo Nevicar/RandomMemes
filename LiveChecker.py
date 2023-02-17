b07b776d85b6bed5055d22e33542368bc5fbc1b6
Nevicar <5368799+Nevicar@users.noreply.github.com>
2023-02-17 15:38:54 -0800

    initial poosh
    very basic and bad live checker for streamers. if live it will initiate a linux (ubuntu) command to download the stream in question. meant to be used as a cronjob type deal.
    
    streamlink is required. be sure to update the folder where you want the data saved in the subprocess.

---
+++ LiveChecker.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import requests
+import subprocess
+
+#variables
+Streamer = 'strimmer' #replace with channel name you want to check
+Quality = '720p60' #reference https://streamlink.github.io/cli.html#cmdoption-arg-STREAM for quality options
+Location = '/folder_path_here/' #reference https://streamlink.github.io/cli.html#cmdoption-o for location options
+StartRecording = f"streamlink -o {Location} https://twitch.tv/{Streamer} {Quality}"
+
+#gets channel information
+StreamerLiveCheck = requests.get('https://www.twitch.tv/' +Streamer).content.decode('utf-8')
+
+#live check/downloader
+if 'isLiveBroadcast' in StreamerLiveCheck:
+    subprocess.Popen(StartRecording, shell=True)
+else:
+    exit()
