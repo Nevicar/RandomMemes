74019b79a2be9851b64018474dc84c977032b00c
Nevicar <5368799+Nevicar@users.noreply.github.com>
2023-02-17 15:08:24 -0800

    initial poosh
    a very bad live checker/downloader for streamers for linux (ubuntu). streamlink is required. this script is meant to be scheduled as a job.

---
+++ LiveChecker.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import requests
+import subprocess
+
+#variables - find/replace the variable below with the streamer you want to check
+Streamer = 'strimmer_name'
+
+#gets channel information
+StreamerLiveCheck = requests.get('https://www.twitch.tv/' +Streamer).content.decode('utf-8')
+
+#live check
+if 'isLiveBroadcast' in StreamerLiveCheck:
+    print('strim is live & recording')
+    subprocess.Popen('streamlink -o /home/ubuntu/testing/{author}/{id}.ts https://www.twitch.tv/strimmer_name 720p60', shell=True)
+else:
+    exit()
