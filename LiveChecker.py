8b902946501fe6c723165afbfd143f905daba0e0
Nevicar <5368799+Nevicar@users.noreply.github.com>
2023-02-17 20:49:26 -0800

    Update LiveChecker.py
    changed Popen to run. no idea why i used that.
    changed quality to best for default.

--- LiveChecker.py
+++ LiveChecker.py
@@ -5,7 +5,7 @@ import subprocess
 
 #variables
 Streamer = 'strimmer' #replace with channel name you want to check
-Quality = '720p60' #reference https://streamlink.github.io/cli.html#cmdoption-arg-STREAM for quality options
+Quality = 'best' #reference https://streamlink.github.io/cli.html#cmdoption-arg-STREAM for quality options
 Location = '/folder_path_here/' #reference https://streamlink.github.io/cli.html#cmdoption-o for location options
 StartRecording = f"streamlink -o {Location} https://twitch.tv/{Streamer} {Quality}"
 
@@ -14,6 +14,6 @@ StreamerLiveCheck = requests.get('https://www.twitch.tv/' +Streamer).content.dec
 
 #live check/downloader
 if 'isLiveBroadcast' in StreamerLiveCheck:
-    subprocess.Popen(StartRecording, shell=True)
+    subprocess.run(StartRecording, shell=True)
 else:
     exit()
